4adb9fa89c49a2a6976b490b02dd03a2dcec8081
BugInMyPP <69634303+Lynx-1ST@users.noreply.github.com>
2024-03-02 15:53:23 +0700

    Update FancyVibrato.js

--- src/commands/Slash/Filters/FancyVibrato.js
+++ src/commands/Slash/Filters/FancyVibrato.js
@@ -24,7 +24,7 @@ module.exports = {
 
         await player.filters.setVibrato(true);
 
-        const embed = new EmbedBuilder().setDescription(`\<a:filter:1200818160828420130>\ | Filter has been set to: \`Vibrato\``).setColor(client.color);
+        const embed = new EmbedBuilder().setDescription(`\<:zcmtyAdmin:1212983944732082246>\ | Bộ lọc: \`Vibrato\``).setColor(client.color);
 
         return interaction.editReply({ embeds: [embed] });
     },
