2c89ef0e34c33d99d3347e7b64ac2b1833c4a3a0
BugInMyPP <2321050132@student.humg.edu.vn>
2024-03-02 20:11:42 +0700

    Update playerDestroy.js

--- src/events/poruEvents/player/playerDestroy.js
+++ src/events/poruEvents/player/playerDestroy.js
@@ -5,28 +5,32 @@ module.exports.run = async (client, player) => {
     if (!channel) return;
 
     // If 247 activated, this will auto connect voice when bot disconnected/destoryed
-    const data = await Reconnect.findOne({ guild: player.guildId });
+    const data = await Reconnect.findOne({guild: player.guildId});
 
     if (data && Date.now() >= data.time) {
         await data.delete();
 
         console.log(`[INFO] 247 has been disabled from (${player.guildId})`);
     } // Disable this "if" when 247 command settings premium is set to "false".
-
     if (data) {
         const voices = client.channels.cache.get(data.voice);
 
-        if (player.state !== "DESTROYING") {
-            await client.poru.createConnection({
-                guildId: data.guild,
-                voiceChannel: data.voice,
-                textChannel: data.text,
-                region: voices.rtcRegion || undefined,
-                deaf: true,
-            });
-        }
+        // Create a timeout to delay the action by a certain amount of time
+        const delay = 60000; // Đặt thời gian chờ 5 giây (5000 milliseconds) - có thể thay đổi tùy ý
+        setTimeout(async () => {
+            if (player.state !== "DESTROYING") {
+                await client.poru.createConnection({
+                    guildId: data.guild,
+                    voiceChannel: data.voice,
+                    textChannel: data.text,
+                    region: voices.rtcRegion || undefined,
+                    deaf: true,
+                });
+            }
+        }, delay);
     }
-    //
+//
 
     console.log(`[DEBUG] Player Destroyed from (${player.guildId})`);
-};
+}
+
