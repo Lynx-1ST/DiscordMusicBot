8080de5856b57ca21661ef78828a6aa3f3430d09
BugInMyPP <69634303+Lynx-1ST@users.noreply.github.com>
2024-03-02 15:01:09 +0700

    Update config.js

--- src/settings/config.js
+++ src/settings/config.js
@@ -5,40 +5,40 @@ module.exports = {
   token: process.env.TOKEN || "MTIwMzc0NDcwNjcwMjYxMDUyMg.GHaZku.jq2kivnDk8FRzWpAiRVwFTqPaaMaxfy6FuBxRw", // <==== PASTE YOU TOKEN
   prefix: process.env.PREFIX || "+", // <==== SET YOU PRERIX BOT [ OWNER COMMANDS ]
   color: process.env.EMBED_COLOR || "#2f3136", // <==== YOU EMBEDED HEX COLOR
-  owner: process.env.OWNER_ID || "1203748123961327707", // <==== BOTS OWNER ID
-  guildLogs: process.env.GUILD_LOGS || "1198586029343518820", // <==== YOUR SERVER JOIN LEFT LOGS CHANNEL ID
-  leaveTimeout: process.env.LEAVE_TIMEOUT || "20000", // <==== SET LEAVE TIMEOUT WHEN BOT WAS ALONE || 1000 = 1sec
+  owner: process.env.OWNER_ID || "1081547609979756596", // <==== BOTS OWNER ID
+  guildLogs: process.env.GUILD_LOGS || "1203748123961327707", // <==== YOUR SERVER JOIN LEFT LOGS CHANNEL ID
+  leaveTimeout: process.env.LEAVE_TIMEOUT || "40000", // <==== SET LEAVE TIMEOUT WHEN BOT WAS ALONE || 1000 = 1sec
   disableYouTube: parseBoolean(process.env.DISABLE_YOUTUBE || "false"), // <==== SET "TRUE OR FALSE" | ENABLE/DISABLE YOUTUBE FEATURES. DISABLING THIS WILL MAKE "AUTOPLAY" COMMANDS USELESS!!!
 
   // ⬇⬇⬇ PORU DETAILS
   playSource: process.env.PLAY_SOURCE || "ytsearch", // <==== SET YOUR PLAY SOURCE || "ytsearch","ytmsearch","scsearch"
   poruOptions: {
     defaultPlatform: process.env.DEFAULT_SOURCE || "ytsearch", // <==== SET DEFAULT SOURCE || "ytsearch","ytmsearch","scsearch"
-    clientID: process.env.SPOTIFY_ID || "e6f84fbec2b44a77bf35a20c5ffa54b8", // <==== SPOTIFY CLIENT ID
-    clientSecret: process.env.SPOTIFY_SECRET || "498f461b962443cfaf9539c610e2ea81", // <==== SPOTIFY CLIENT SECRET
+    clientID: process.env.SPOTIFY_ID || "e35788ef31ae406ba4acf67293417cdd", // <==== SPOTIFY CLIENT ID
+    clientSecret: process.env.SPOTIFY_SECRET || "713fcc61c58e418e92b44fe05a25e30c", // <==== SPOTIFY CLIENT SECRET
     reconnectTries: 5, // <==== TOTAL ATTEMPS TO TRY IF RECONNECT FAILED. YOU CAN CHANGE IT TO "Infinity" FOR UNLIMITED ATTEMPS.
     playlistLimit: 2, // <==== 1 = 100 TRACKS
     albumLimit: 2, // <==== 1 = 50 TRACKS
     artistLimit: 2, // <==== 1 = 50 TRACKS
-    searchMarket: "us",
+    searchMarket: "vi",
   },
   nodes: [
     {
       name: process.env.NODE_NAME1 || "Main", 
       host: process.env.NODE_HOST1 || "lava.link", 
       port: parseInt(process.env.NODE_PORT1 || "80"), 
-      password: process.env.NODE_PASSWORD1 || "LAVA", 
+      password: process.env.NODE_PASSWORD1 || "kronix", 
       secure: parseBoolean(process.env.NODE_SECURE1 || "false"),
       regions: process.env.NODE_REGIONS1 || ["singapore"], 
     }
   ],
 
 
-  mongoUri: process.env.MONGO_URI || "mongodb+srv://pee33:wnn99e@cluster0.fgivnid.mongodb.net/?retryWrites=true&w=majority", 
-  supportUrl: process.env.SUPPORT_URL || "https://discord.gg/7MEHnM6T87", 
-  voteUrl: process.env.VOTE_URL || "https://discord.gg/7MEHnM6T87",
-  inviteUrl: process.env.INVITE_URL || "https://discord.com/api/oauth2/authorize?client_id=1197585882803343391&permissions=40271764260928&scope=bot%20applications.commands", 
-  imageUrl: process.env.IMAGE_URL || "https://media.discordapp.net/attachments/1162795987014787162/1197198433962971156/pngtree-pair-of-headphones-on-the-water-at-nighttime-image_2931863.png?ex=65c39f6d&is=65b12a6d&hm=e94858f4cbfc92ee4868da52953a4015b2c2703c5f0e12f2e144d446be72d8fe&=&format=webp&quality=lossless&width=800&height=448",
+  mongoUri: process.env.MONGO_URI || "mongodb+srv://maihang987th:loc.1005@discord.xhm84ds.mongodb.net/?retryWrites=true&w=majority", 
+  supportUrl: process.env.SUPPORT_URL || "https://github.com/Lynx-1ST", 
+  voteUrl: process.env.VOTE_URL || "https://github.com/Lynx-1ST",
+  inviteUrl: process.env.INVITE_URL || "https://discord.com/api/oauth2/authorize?client_id=1203744706702610522&permissions=44944789462848&scope=bot%20applications.commands", 
+  imageUrl: process.env.IMAGE_URL || "https://imgs.search.brave.com/jwLHIDhA6haNZYN37vbglCjO18R8DhLrrryE349pjQY/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9naWZk/Yi5jb20vaW1hZ2Vz/L2hpZ2gvYW5pbWUt/YmFubmVyLWdpZi1m/aWxlLTQ3N2tiLXU5/OHhoYnZqeXppM2gz/YWouZ2lm.gif",
 };
 
 function parseBoolean(value) {
