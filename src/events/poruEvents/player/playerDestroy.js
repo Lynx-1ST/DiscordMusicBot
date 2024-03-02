dc5f0bbe6f8a1dce99274692d8c799fc512c1e43
BugInMyPP <2321050132@student.humg.edu.vn>
2024-03-02 20:38:04 +0700

    1

--- src/events/poruEvents/player/playerDestroy.js
+++ src/events/poruEvents/player/playerDestroy.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, player) => {
     const channel = client.channels.cache.get(player.textChannel);
     if (!channel) return;
 
-    // If 247 activated, this will auto connect voice when bot disconnected/destoryed
+    // If 247 activated, this will auto connect voice when bot disconnected/destroyed
     const data = await Reconnect.findOne({ guild: player.guildId });
 
     if (data && Date.now() >= data.time) {
@@ -12,11 +12,12 @@ module.exports.run = async (client, player) => {
 
         console.log(`[INFO] 247 has been disabled from (${player.guildId})`);
     } // Disable this "if" when 247 command settings premium is set to "false".
+
     if (data) {
         const voices = client.channels.cache.get(data.voice);
 
         // Create a timeout to delay the action by a certain amount of time
-        const delay = 60000; // Đặt thời gian chờ 5 giây (5000 milliseconds) - có thể thay đổi tùy ý
+        const delay = 5000; // Đặt thời gian chờ 5 giây (5000 milliseconds) - có thể thay đổi tùy ý
         setTimeout(async () => {
             if (player.state !== "DESTROYING") {
                 await client.poru.createConnection({
@@ -29,8 +30,7 @@ module.exports.run = async (client, player) => {
             }
         }, delay);
     }
-//
+    //
 
     console.log(`[DEBUG] Player Destroyed from (${player.guildId})`);
-}
-
+};
--- src/events/poruEvents/player/queueEnd.js
+++ src/events/poruEvents/player/queueEnd.js
@@ -10,24 +10,20 @@ module.exports.run = async (client, player) => {
     if (player.message) await player.message.delete();
 
     // this will make the bot will not be disconneted/destroyed when queue end if 247 activated
-    const data = await Reconnect.findOne({guild: player.guildId});
+    const data = await Reconnect.findOne({ guild: player.guildId });
 
     if (data && Date.now() >= data.time) {
         await data.delete();
     } // Disable this "if" when 247 command settings premium is set to "false".
-    if (data) {
-        // Create a timeout to delay the action by a certain amount of time
-        const delay = 60000; // Đặt thời gian chờ 5 giây (5000 milliseconds) - có thể thay đổi tùy ý
-        setTimeout(async () => {
-            await player.destroy();
 
-            const embed = new EmbedBuilder()
-                .setDescription(`\`👋\` | Đã ngắt kết nối...!!! Do hàng đợi trống. Điều này có thể được vô hiệu hóa bằng cách sử dụng lệnh \`247\`.`)
-                .setColor(client.color);
+    if (data) return;
+    //
 
-            channel.send({embeds: [embed]});
-        }, delay);
-    }
-}
-//
+    await player.destroy();
 
+    const embed = new EmbedBuilder()
+        .setDescription(`\`👋\` | Đã ngắt kết nối...!!! Do hàng đợi trống. Điều này có thể được vô hiệu hóa bằng cách sử dụng lệnh \`247\`.`)
+        .setColor(client.color);
+
+    return channel.send({ embeds: [embed] });
+};
