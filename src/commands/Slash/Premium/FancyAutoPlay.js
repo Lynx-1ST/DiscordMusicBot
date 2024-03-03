a0aa553966cde19a59bcf623041c9ce64a7611f7
BugInMyPP <2321050132@student.humg.edu.vn>
2024-03-03 21:45:04 +0700

    Update FancyAutoPlay.js

--- src/commands/Slash/Premium/FancyAutoPlay.js
+++ src/commands/Slash/Premium/FancyAutoPlay.js
@@ -18,28 +18,35 @@ module.exports = {
         premium: true,
     },
     run: async (client, interaction) => {
-        await interaction.deferReply({ ephemeral: true });
+        await interaction.deferReply({ephemeral: true});
 
         const player = client.poru.players.get(interaction.guild.id);
 
+        if (!player || !player.currentTrack) {
+            const embed = new EmbedBuilder().setDescription(`\✖️\ | Hiện không có bài hát nào đang phát!`).setColor(client.color);
+            return interaction.editReply({embeds: [embed]});
+        }
+
         const currentsong = player.currentTrack.info;
+
+        if (!currentsong || !currentsong.uri) {
+            const embed = new EmbedBuilder().setDescription(`\✖️\ | Không thể lấy thông tin của bài hát hiện tại!`).setColor(client.color);
+            return interaction.editReply({embeds: [embed]});
+        }
 
         const ytUri = /^(https?:\/\/)?(www\.)?(m\.)?(music\.)?(youtube\.com|youtu\.?be)\/.+$/gi.test(currentsong.uri);
 
         if (!ytUri) {
             const embed = new EmbedBuilder().setDescription(`\✖️\ | Tính năng tự động phát chỉ hỗ trợ YouTube!`).setColor(client.color);
-
-            return interaction.editReply({ embeds: [embed] });
+            return interaction.editReply({embeds: [embed]});
         }
 
         if (player.autoplay === true) {
             player.autoplay = false;
-
             await player.queue.clear();
 
             const embed = new EmbedBuilder().setDescription(`\`🔴\` | Tự động phát đã: \`TẮT\``).setColor(client.color);
-
-            return interaction.editReply({ embeds: [embed] });
+            return interaction.editReply({embeds: [embed]});
         } else {
             player.autoplay = true;
 
@@ -55,9 +62,8 @@ module.exports = {
                 await player.queue.add(res.tracks[Math.floor(Math.random() * res.tracks.length) ?? 1]);
 
                 const embed = new EmbedBuilder().setDescription(`\`🔵\` | Tự động phát đã: \`BẬT\``).setColor(client.color);
-
-                return interaction.editReply({ embeds: [embed] });
+                return interaction.editReply({embeds: [embed]});
             }
         }
-    },
-};
+    }
+}
