c7d2e61448fdef2b7b0558c9539adb1dce129da2
BugInMyPP <2321050132@student.humg.edu.vn>
2024-03-08 11:01:46 +0700

    Update trackEnd.js

--- src/events/poruEvents/player/trackEnd.js
+++ src/events/poruEvents/player/trackEnd.js
@@ -8,23 +8,22 @@ module.exports.run = async (client, player) => {
     if (player.autoplay === true) {
         try {
             const trackSearch = player.currentTrack.info;
-
             const ytUri = /^(https?:\/\/)?(www\.)?(m\.)?(music\.)?(youtube\.com|youtu\.?be)\/.+$/gi.test(trackSearch.uri);
-
             if (ytUri) {
                 const source = client.config.playSource;
                 const identifier = trackSearch.identifier;
                 const search = `https://music.youtube.com/watch?v=${identifier}&list=RD${identifier}`;
                 const res = await client.poru.resolve(search, source);
-
                 for (const track of res.tracks) {
                     track.info.requester = trackSearch.requester;
                 }
-
                 await player.queue.add(res.tracks[Math.floor(Math.random() * res.tracks.length) ?? 2]);
             }
         } catch (error) {
-            ///
+            console.error("Error fetching YouTube track:", error);
+            // Xử lý lỗi ở đây, ví dụ: thông báo cho người dùng hoặc thực hiện hành động khác
+            const embed = new EmbedBuilder().setDescription(`\`❌\` | Đã xảy ra lỗi khi tải bài hát: \`${error.message}\``).setColor(client.color);
+            return channel.send({ embeds: [embed] });
         }
     }
 };
