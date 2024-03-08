cac68156bf9d035a032d15bedd2b3e42e8f5c41b
BugInMyPP <2321050132@student.humg.edu.vn>
2024-03-08 14:07:49 +0700

    Update queueEnd.js

--- src/events/poruEvents/player/queueEnd.js
+++ src/events/poruEvents/player/queueEnd.js
@@ -9,7 +9,7 @@ module.exports.run = async (client, player) => {
 
     if (player.message) await player.message.delete();
 
-    // this will make the bot will not be disconneted/destroyed when queue end if 247 activated
+    // This will prevent the bot from being disconnected/destroyed when the queue ends if 247 is activated
     const data = await Reconnect.findOne({ guild: player.guildId });
 
     if (data && Date.now() >= data.time) {
@@ -17,18 +17,26 @@ module.exports.run = async (client, player) => {
     } // Disable this "if" when 247 command settings premium is set to "false".
 
     if (data) return;
-    //
 
     // Add a delay before disconnecting
-    const disconnectDelay = 90000; // Delay in milliseconds (5 seconds in this example)
+    const disconnectDelay = 90000; // Delay in milliseconds (90 seconds in this example)
+
+    const embed = new EmbedBuilder()
+        .setDescription(`\`⏳\` | Đang chờ ${disconnectDelay / 1000} giây trước khi ngắt kết nối...`)
+        .setColor(client.color);
+
+    const waitingMessage = await channel.send({ embeds: [embed] });
 
     setTimeout(async () => {
         await player.destroy();
 
-        const embed = new EmbedBuilder()
+        const finalEmbed = new EmbedBuilder()
             .setDescription(`\`👋\` | Đã ngắt kết nối...!!! Do hàng đợi trống. Điều này có thể được vô hiệu hóa bằng cách sử dụng lệnh \`247\`.`)
             .setColor(client.color);
 
-        return channel.send({ embeds: [embed] });
+        // Delete the waiting message after disconnecting
+        await waitingMessage.delete();
+
+        return channel.send({ embeds: [finalEmbed] });
     }, disconnectDelay);
 };
