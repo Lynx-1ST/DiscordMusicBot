88a87ef2c449dd72da44df8403126067f3b61118
BugInMyPP <2321050132@student.humg.edu.vn>
2024-03-08 18:02:53 +0700

    Update queueEnd.js

--- src/events/poruEvents/player/queueEnd.js
+++ src/events/poruEvents/player/queueEnd.js
@@ -9,47 +9,33 @@ module.exports.run = async (client, player) => {
 
     if (player.message) await player.message.delete();
 
-    // this will make the bot will not be disconnected/destroyed when queue end if 247 activated
+    // Kiểm tra nếu đã cài đặt 247 và đã đến thời gian ngắt kết nối
     const data = await Reconnect.findOne({ guild: player.guildId });
-
     if (data && Date.now() >= data.time) {
         await data.delete();
-    } // Disable this "if" when 247 command settings premium is set to "false".
+    }
 
+    // Nếu có cài đặt 247, không thực hiện ngắt kết nối
     if (data) return;
-    //
 
-    // Add a delay before disconnecting
-    const disconnectDelay = 90000; // Delay in milliseconds (5 seconds in this example)
-
+    // Thêm thông báo thời gian chờ trước khi ngắt kết nối
+    const disconnectDelay = 90000; // 90 giây
     const embed = new EmbedBuilder()
         .setDescription(`\`⏳\` | Đang chờ ${disconnectDelay / 1000} giây trước khi ngắt kết nối...`)
         .setColor(client.color);
-
     const waitingMessage = await channel.send({ embeds: [embed] });
 
-    const timer = setTimeout(async () => {
+    // Đặt hẹn giờ để ngắt kết nối sau thời gian chờ
+    setTimeout(async () => {
         await player.destroy();
 
-        const finalEmbed = new EmbedBuilder()
-            .setDescription(`\`👋\` | Đã ngắt kết nối sau ${disconnectDelay / 1000} giây. Hàng đợi trống. Điều này có thể được vô hiệu hóa bằng cách sử dụng lệnh \`247\`.`)
-            .setColor(client.color);
-
-        // Delete the waiting message after disconnecting
+        // Xoá thông báo chờ đợi sau khi ngắt kết nối
         await waitingMessage.delete();
+
+        // Gửi thông báo sau khi ngắt kết nối
+        const finalEmbed = new EmbedBuilder()
+            .setDescription(`\`👋\` | Đã ngắt kết nối...!!! Do hàng đợi trống. Điều này có thể được vô hiệu hóa bằng cách sử dụng lệnh \`247\`.`)
+            .setColor(client.color);
         return channel.send({ embeds: [finalEmbed] });
     }, disconnectDelay);
-
-    // Listen for new tracks added to the queue
-    player.on("trackAdd", async () => {
-        // If a new track is added, cancel the timer and delete the waiting message
-        clearTimeout(timer);
-        await waitingMessage.delete();
-    });
-
-    // Listen for queue end
-    player.on("queueEnd", async () => {
-        // If the queue ends prematurely, delete the waiting message
-        await waitingMessage.delete();
-    });
 };
