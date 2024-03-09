439204f03e1b88ece44fcbe226e404f343a9208c
BugInMyPP <2321050132@student.humg.edu.vn>
2024-03-09 15:04:33 +0700

    2

--- src/settings/models/Control.js
+++ src/settings/models/Control.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const CreateControl = mongoose.Schema({
     guild: { type: String, required: true },
-    playerControl: { type: String, default: "enable" },
+    playerControl: { type: String, default: "disable" },
 });
 
 module.exports = mongoose.model("Control", CreateControl);
